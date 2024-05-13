1455b42d655ee7f80d0e98e3b6634bd95da6423a
Porama Arsama <porama.prm@gmail.com>
2024-05-13 15:02:07 +0700

    Update App.jsx

--- src/view/App.jsx
+++ src/view/App.jsx
@@ -1,27 +1,48 @@
-import { Card, Input, Layout, Table, theme } from "antd";
+import { Card, Layout, Table } from "antd";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import Footer from "../components/Partials/Footer";
 import Header from "../components/Partials/Header";
 import SideBar from "../components/Partials/SideBar";
 import { env } from "../env";
-const { Search } = Input;
-
-const url = `${env.service_url}`;
 
 const { Content } = Layout;
+const url = `${env.service_url}`;
+
+const columnsClass = [
+  {
+    title: "เลขที่ห้อง",
+    dataIndex: "room_number",
+    key: "room_number",
+  },
+  {
+    title: "ห้องเรียน",
+    dataIndex: "room_name",
+    key: "room_name",
+  },
+  {
+    title: "จำนวนนักเรียนในห้อง",
+    dataIndex: "class_maps",
+    render: (classMaps) => classMaps.length,
+  },
+];
+
+const columnStudents = [
+  {
+    title: "เลขประจำตัวนักเรียน",
+    dataIndex: "student_code",
+    key: "student_code",
+  },
+  {
+    title: "ชื่อจริง - นามสกุล",
+    render: (text, record) => `${record.title} ${record.fname} ${record.lname}`,
+  },
+];
 
 const App = () => {
-  const [dataSearch, setDataSearch] = useState("");
-
-  const [isSearch, setIsSearch] = useState(false);
-
-  const {
-    token: { colorBgContainer, borderRadiusLG },
-  } = theme.useToken();
-
   const [dataStudent, setDataStudent] = useState({});
   const [dataClassroom, setDataClassroom] = useState({});
+  const [loading, setLoading] = useState(false);
 
   const fetchStudentAndClassroomByGradeLevel = async (level) => {
     try {
@@ -51,57 +72,22 @@ const App = () => {
   };
 
   useEffect(() => {
-    for (let i = 1; i <= 3; i++) {
-      fetchStudentAndClassroomByGradeLevel(i);
-    }
+    const fetchData = async () => {
+      setLoading(true);
+      for (let i = 1; i <= 3; i++) {
+        await fetchStudentAndClassroomByGradeLevel(i);
+      }
+      setLoading(false);
+    };
+    fetchData();
   }, []);
-
-  const columnsClass = [
-    {
-      title: "เลขที่ห้อง",
-      dataIndex: "room_number",
-      key: "room_number",
-    },
-    {
-      title: "ห้องเรียน",
-      dataIndex: "room_name",
-      key: "room_name",
-    },
-    {
-      title: "จำนวนนักเรียนในห้อง",
-      dataIndex: "class_maps",
-      render: (classMaps) => classMaps.length,
-    },
-  ];
-
-  const columnStudents = [
-    {
-      title: "เลขประจำตัวนักเรียน",
-      dataIndex: "student_code",
-      key: "student_code",
-    },
-    {
-      title: "ชื่อจริง - นามสกุล",
-      render: (text, record) =>
-        `${record.title} ${record.fname} ${record.lname}`,
-    },
-  ];
 
   return (
     <Layout hasSider>
       <SideBar />
-      <Layout
-        style={{
-          marginLeft: 200,
-        }}
-      >
+      <Layout style={{ marginLeft: 200 }}>
         <Header />
-        <Content
-          style={{
-            margin: "24px 16px 0",
-            overflow: "initial",
-          }}
-        >
+        <Content style={{ margin: "24px 16px 0", overflow: "initial" }}>
           <Card title="Summary Report">
             {[1, 2, 3].map((level) => (
               <Card
@@ -113,17 +99,28 @@ const App = () => {
                 }}
               >
                 <div
-                  style={{ display: "flex", justifyContent: "space-between" }}
+                  style={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                  }}
                 >
                   <Table
                     style={{ width: "49%" }}
                     columns={columnStudents}
                     dataSource={dataStudent[`dataStudentGrade${level}`]}
+                    pagination={{
+                      pageSize: 4, // จำนวนรายการต่อหน้า
+                    }}
+                    loading={loading}
                   />
                   <Table
                     style={{ width: "49%" }}
                     columns={columnsClass}
                     dataSource={dataClassroom[`dataClassroomGrade${level}`]}
+                    pagination={{
+                      pageSize: 4, // จำนวนรายการต่อหน้า
+                    }}
+                    loading={loading}
                   />
                 </div>
               </Card>
